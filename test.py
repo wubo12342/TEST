de8fb4acec0eccc31955bfd695466633a85f1e18
wuboiy <wubaiyi379@gmail.com>
2025-09-01 00:22:27 +0800

    靜態爬蟲

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://www.showtimes.com.tw/programs')
+html = response.content.decode("utf-8")  # ✅ 這樣才會真的解碼
+
+print(html)
