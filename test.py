3cf9ed3a98d3dfa1b5979ac2919597ae53c2a858
wuboiy <wubaiyi379@gmail.com>
2025-09-01 15:23:59 +0800

    印鬼滅在哪個影城有

--- test.py
+++ test.py
@@ -3,13 +3,17 @@ import time
 from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome()  # 不用指定路徑，因為就在同一個資料夾
-driver.get("https://www.showtimes.com.tw/programs")
+driver.get("https://www.showtimes.com.tw/programs/11847")
 
 time.sleep(5) 
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-names = soup.find_all('div', {'class':'sc-gmPhUn iQHZmV'})
+names = soup.find_all('div', {'class':'sc-kAkpmW efezeh'})
 
 for name in names:
-    print(name.text)
+    text = name.text.strip()  # 去掉前後空白
+    if "秀泰" in text:
+        cinemas = text.split("，")
+        for cinema in cinemas:
+            print(cinema)
