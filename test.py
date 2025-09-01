62815aa7fb949f7ff325e332ed43f28320dd20de
wuboiy <wubaiyi379@gmail.com>
2025-09-01 15:07:17 +0800

    爬到目前上映的的電影

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-import requests
+from selenium import webdriver
+import time
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.showtimes.com.tw/programs')
-html = response.content.decode("utf-8")  # ✅ 這樣才會真的解碼
+driver = webdriver.Chrome()  # 不用指定路徑，因為就在同一個資料夾
+driver.get("https://www.showtimes.com.tw/programs")
 
-print(html)
+time.sleep(5) 
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+names = soup.find_all('div', {'class':'sc-gmPhUn iQHZmV'})
+
+for name in names:
+    print(name.text)
