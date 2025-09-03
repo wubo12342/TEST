d0b2b85ce37f639b048f79385ea98887944776e3
wuboiy <wubaiyi379@gmail.com>
2025-09-03 22:20:37 +0800

    爬蟲練習:爬取價格 書名 評價

--- test.py
+++ test.py
@@ -1,54 +1,20 @@
-import mysql.connector
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import requests
 from bs4 import BeautifulSoup
-import time
 
-# 連線到 MySQL (XAMPP)
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",      # 預設 root
-    password="",      # XAMPP 預設密碼通常是空的
-    database="movieDB"
-)
-cursor = conn.cursor()
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/139.0.0.0 Safari/537.36"
+}
 
-driver = webdriver.Chrome()
-driver.get("https://www.showtimes.com.tw/programs")
-time.sleep(5)
+url = "https://books.toscrape.com/"
+res = requests.get(url, headers=headers).text
+soup = BeautifulSoup(res, 'html.parser')
 
-for i in range(2):  # 測試爬兩部電影
-    movies = driver.find_elements(By.CSS_SELECTOR, "a.sc-iGgWBj")
-    movies[i].click()
-    time.sleep(3)
+books = soup.select('ol.row li')
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-
-    # 抓標題
-    try:
-        title = soup.find("h1", {"class": "sc-dyGzUR"}).text.strip()
-    except:
-        title = "未找到標題"
-
-    infos = soup.find_all("div", {"class": "sc-kAkpmW efezeh"})
-    labels = ["length", "release_date", "theaters", "genre", "actors", "director", "plot"]
-    values = [info.text.strip() for info in infos[1:]]
-
-    while len(values) < len(labels):
-        values.append("")
-
-    # 存入 MySQL
-    cursor.execute("""
-    INSERT INTO movies (title, length, release_date, theaters, genre, actors, director, plot)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-    """, (title, *values))
-    conn.commit()
-
-    print(f"✅ 已存入 MySQL：{title}")
-    driver.back()
-    time.sleep(3)
-
-driver.quit()
-cursor.close()
-conn.close()
+for book in books:
+    name = book.select_one('h3 a')['title'] 
+    rating = book.select_one('p')['class']
+    price = book.select_one('p.price_color').text
+    print(f"Name : {name}\nRating : {rating[1]}\nPrice : {price}")
